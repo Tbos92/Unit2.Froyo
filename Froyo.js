35a9a8b270ae3a86ab2ad8333e74f120677da22a
Tyler <tbos92@gmail.com>
2024-08-21 21:39:09 -0400

    Froyo Unit 2 final push

---
+++ Froyo.js
@@ -0,0 +1,35 @@
+// Prompt user for froyo flavors.
+const flavors = prompt(
+  "Please enter a list of froyo flavors separated by commas."
+);
+
+// convert user input into an array of individual flavors
+const flavorsArray = flavors.split(",");
+console.log(flavorsArray);
+
+// create an object to track flavors that have been observed
+const froyo = {};
+
+// create a function that loops over array and
+// determines if flavor exists and increase count
+function trackfroyoflavors() {
+  for (let i = 0; i < flavorsArray.length; i++) {
+    // does specified key exist in froyo object?
+    // trim and convert to lowercase to insure duplicate
+    // entries are not counted as separate keys.
+
+    // if specific key exists, increase key value by 1
+    if (froyo[flavorsArray[i].trim().toLowerCase()]) {
+      froyo[flavorsArray[i].trim().toLowerCase()] += 1;
+    } else {
+      // If the key doesn't exist, create it with value 1
+      froyo[flavorsArray[i].trim().toLowerCase()] = 1;
+    }
+  }
+  // return the completed object
+  return froyo;
+}
+// call the function to track the flavors
+const flavorcounts = trackfroyoflavors();
+console.log(flavorcounts);
+
